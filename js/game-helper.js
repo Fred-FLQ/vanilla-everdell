602797bc3fb15314f0eb722b1de6d40eb775f66b
Fred - FLQ <strapi.0txni@aleeas.com>
2024-10-23 11:30:53 +0200

    Add empty deck and card.count undefined handling

--- js/game-helper.js
+++ js/game-helper.js
@@ -23,12 +23,20 @@ function drawRandomCards(cardsQuantity) {
     let randomCards = [];
 
     for (let i = 1; i <= cardsQuantity; i++) {
+        // Handling empty deck
+        if (gameState.mainDeck.length === 0) {
+            throw new Error('Deck is empty');
+        }
+
         let randomNumber = Math.floor(Math.random() * gameState.mainDeck.length);
         let randomCard = gameState.mainDeck[randomNumber];
 
         // Accessing count property of randomCard - [NEW] Easier to use the static method Object.values()
         for (let key in randomCard) {
             let cardData = randomCard[key];
+
+            if (cardData.count === undefined) cardData.count = 1; // Edge Case Handling: corrupted initial data
+
             if (cardData.count === 1) {
                 gameState.mainDeck.splice(randomNumber, 1);
             } else {
