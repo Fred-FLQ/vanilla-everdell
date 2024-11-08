66277c4578bfa577a193f7129331b78dadadca4a
Fred - FLQ <strapi.0txni@aleeas.com>
2024-11-08 16:19:54 +0100

    Deprecate renderPlayerWorkers(), Add renderCounter(), gameState.basicActionSpaces, placeWorker(), getResources()

--- js/dom-helper.js
+++ js/dom-helper.js
@@ -17,6 +17,10 @@ export const gameState = {
             pebble: 0,
             berry: 0
         }
+    },
+    basicActionSpaces: {
+        threeTwig: 0,
+        twoTwigOneCard: 0
     }
 };
 
@@ -44,9 +48,10 @@ window.gameState = gameState; // [NEW] Make gameState globally accessible
 
 const gameInit = async () => {
     await fetchCardsData();
-    console.log(gameState.mainDeck.length);
+
     // Place 8 random cards in meadow
     gameState.meadow = drawRandomCards(8);
+
     // Player gets 2 workers
     gameState.player.workers = 2;
 
@@ -54,20 +59,25 @@ const gameInit = async () => {
     gameState.player.hand = drawRandomCards(5);
 }
 
-const renderPlayerWorkers = async () => {
-    const playerWorkersElem = document.querySelector('#player-workers span');
-    playerWorkersElem.innerHTML = gameState.player.workers;
+// New generic function to render counters
+const renderCounter = async (counterState, containerElem) => {
+    const counterElement = containerElem;
+    counterElement.textContent = counterState;
 }
 
 // New function to render cards in different context
 const renderCards = async (cardsArray, containerElem) => {
+    containerElem.innerHTML = ''; // Clear container before loading cards
     const renderCardsElem = containerElem;
     cardsArray.forEach(card => {
         const cardContainer = renderCardsElem.appendChild(document.createElement("article"));
         cardContainer.classList.add("card");
-        const cardCostHTML = Object.keys(card.cost).map(ressource => {
-            return `<li>${ressource}: ${card.cost[ressource]}</li>`;
-        }).join(''); // Remove ','
+        const cardCostHTML = Object.keys(card.cost)
+            .map(ressource => {
+                return `<li>${ressource}: ${card.cost[ressource]}&nbsp;</li>`;
+            })
+            .join(''); // Remove ','
+        card.produces = null; // Remove the "produces" part of the card for now
         cardContainer.innerHTML = `
             <header>
                 <div class="category">${card.category}</div>
@@ -86,8 +96,38 @@ const renderCards = async (cardsArray, containerElem) => {
     })
 };
 
+const placeWorker = async (location) => {
+    if (gameState.player.workers > 0) {
+        gameState.player.workers -= 1;
+        gameState.basicActionSpaces[location] += 1;
+        renderCounter(gameState.player.workers, document.querySelector('#player-workers span'));
+        renderCounter(gameState.basicActionSpaces[location], document.querySelector(`#${location} span`));
+        getResources(location);
+    } else {
+        alert("You don't have enough workers.");
+    }
+}
+
+const getResources = async (location) => {
+    switch (location) {
+        case 'threeTwig':
+            gameState.player.resources.twig += 3;
+            renderCounter(gameState.player.resources.twig, document.querySelector('#twigs span'));
+            break;
+        case 'twoTwigOneCard':
+            gameState.player.resources.twig += 2;
+            renderCounter(gameState.player.resources.twig, document.querySelector('#twigs span'));
+            const newCards = drawRandomCards(1);
+            newCards.forEach(card => gameState.player.hand.length < 8 ? gameState.player.hand.push(card) : alert('Maximum of 8 cards in hand.'));
+            renderCards(gameState.player.hand, document.querySelector('#player-hand .cards-grid'));
+            break;
+    }
+}
+
 gameInit();
 
 // For testing
-window.renderPlayerWorkers = renderPlayerWorkers;
+// window.renderPlayerWorkers = renderPlayerWorkers;
+window.renderCounter = renderCounter;
 window.renderCards = renderCards;
+window.placeWorker = placeWorker;
