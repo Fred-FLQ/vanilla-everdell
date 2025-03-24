871552318b3904e592470bdebb47cd71fa531496
Fred - FLQ <strapi.0txni@aleeas.com>
2025-03-24 10:44:42 +0100

    Update replenishMeadow() to IDB, Deprecate drawRandomCards()

--- js/cards-handling.js
+++ js/cards-handling.js
@@ -1,46 +1,16 @@
 import { gameState } from "./game-state.js";
-import { getDeckLength } from "./everdell-idb.js";
-
-const drawRandomCards = cardsQuantity => {
-    let randomCards = [];
-
-    for (let i = 1; i <= cardsQuantity; i++) {
-        // Handling empty deck
-        if (gameState.mainDeck.length === 0) {  // [IDB] Needs an IDB.count() transaction
-            throw new Error('Deck is empty');
-        }
-
-        // Get a random card
-        let randomNumber = Math.floor(Math.random() * gameState.mainDeck.length);
-        let randomCard = gameState.mainDeck[randomNumber]; // [IDB] Needs an IDB cursor + advance(randomNumber)
-
-        // Edge Case Handling: corrupted initial data
-        if (randomCard.count === undefined) randomCard.count = 1;
-
-        if (randomCard.count === 1) {
-            gameState.mainDeck.splice(randomNumber, 1); // Remove the card from deck if only 1 of a kind left
-        } else {
-            randomCard.count -= 1;
-        }
-        // Add a unique ID to drawn card
-        let drawnCard = { ...randomCard, id: crypto.randomUUID() };
-
-        randomCards.push(drawnCard);
-    }
-    return randomCards;
-}
+import { drawFromDeck, getDeckLength, getLocationLength } from "./everdell-idb.js";
 
 // Add cards to area
 const addCardToArea = (card, area) => {
     area.push(card);
 }
 
-// Replenish Meadow
-function replenishMeadow() {
-    if (gameState.meadow.length < 8) {
-        let newCard = drawRandomCards(1);
-        newCard.forEach(card => addCardToArea(card, gameState.meadow));
+async function replenishMeadow() {
+    let meadowLength = await getLocationLength('meadow');
+    if (meadowLength < 8) {
+        await drawFromDeck('main-deck', 'cards', 1, 'meadow');
     }
-}
+};
 
-export { drawRandomCards, addCardToArea, replenishMeadow };
+export { addCardToArea, replenishMeadow };
